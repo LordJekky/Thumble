1c1d4c94c31fe5850e44379bf44c04b3fb6455e2
LordJekky <george.quinn@gmail.com>
2018-07-12 10:42:58 -0700

    Changed UI screen transition animations

--- Assets/Scripts/UIBehaviour.cs
+++ Assets/Scripts/UIBehaviour.cs
@@ -10,20 +10,72 @@ enum UIAnimationStates
     OpeningPanel
 }
 
+class FadingUIObject
+{
+    GameObject UIObject;
+    Image ImgComp;
+
+    public bool Completed;
+    public bool FadeIn;
+    public float Speed;
+
+    Color OriginalColour;
+
+    public FadingUIObject(GameObject UIObject, bool FadeIn, float Speed)
+    {
+        this.UIObject = UIObject;
+        this.FadeIn = FadeIn;
+        this.Speed = Speed;
+
+        ImgComp = UIObject.GetComponent<Image>();
+        OriginalColour = ImgComp.color;
+
+        if (FadeIn)
+        {
+            Color c = ImgComp.color;
+            c.a = 0;
+            ImgComp.color = c;
+        }
+    }
+
+    public void Dispose()
+    {
+        ImgComp.color = OriginalColour;
+    }
+
+    public void Update()
+    {
+        Color c = ImgComp.color;
+
+        if (FadeIn)
+        {
+            c.a += Speed;
+            if (c.a == OriginalColour.a) Completed = true;
+        }
+        else
+        {
+            c.a -= Speed;
+            if (c.a <= 0) Completed = true;
+        }
+
+        ImgComp.color = c;
+    }
+}
+
 class MovingUIObject
 {
     GameObject UIObject;
-    float TargetY;
+    Vector2 Target;
     float Speed;
 
     Vector3 OriginalPosition;
 
     public bool Completed;
 
-    public MovingUIObject(GameObject UIObject, float TargetY, float Speed)
+    public MovingUIObject(GameObject UIObject, Vector2 Target, float Speed)
     {
         this.UIObject = UIObject;
-        this.TargetY = TargetY;
+        this.Target = Target;
         this.Speed = Speed;
 
         OriginalPosition = UIObject.transform.localPosition;
@@ -38,20 +90,34 @@ class MovingUIObject
     {
         Vector3 Pos = UIObject.transform.localPosition;
 
-        if (Pos.y < TargetY)
+        //moving each aspect individually for now - If objects need to move at a diagonal later
+        //change this to use vectors for better appearance.
+
+        if (Pos.y < Target.y)
         {
             Pos.y += Speed;
-            if (Pos.y > TargetY) Pos.y = TargetY;
+            if (Pos.y > Target.y) Pos.y = Target.y;
         }
-        else if (Pos.y > TargetY)
+        else if (Pos.y > Target.y)
         {
             Pos.y -= Speed;
-            if (Pos.y < TargetY) Pos.y = TargetY;
+            if (Pos.y < Target.y) Pos.y = Target.y;
+        }
+
+        if (Pos.x < Target.x)
+        {
+            Pos.x += Speed;
+            if (Pos.x > Target.x) Pos.x = Target.x;
+        }
+        else if (Pos.x > Target.x)
+        {
+            Pos.x -= Speed;
+            if (Pos.x < Target.x) Pos.x = Target.x;
         }
 
         UIObject.transform.localPosition = Pos;
 
-        if (Pos.y == TargetY) Completed = true;
+        if (Pos.y == Target.y && Pos.x == Target.x) Completed = true;
     }
 }
 
@@ -59,6 +125,9 @@ public class UIBehaviour : MonoBehaviour
 {
     List<MovingUIObject> MovingObjects;
     List<MovingUIObject> FinishedObjects;
+
+    List<FadingUIObject> FadingObjects;
+    List<FadingUIObject> FinishedFadingObjects;
 
     Dictionary<string, GameObject> UIPanels;
 
@@ -82,9 +151,56 @@ public class UIBehaviour : MonoBehaviour
         MovingObjects = new List<MovingUIObject>();
         FinishedObjects = new List<MovingUIObject>();
 
+        FadingObjects = new List<FadingUIObject>();
+        FinishedFadingObjects = new List<FadingUIObject>();
+
         State = UIAnimationStates.Default;
 	}
-	
+
+    //returns the location which this object should slide to or from
+    //when animating screen transitions
+    public Vector2 GetObjectSlideTarget(GameObject O)
+    {
+        float X = O.transform.localPosition.x;
+        float Y = O.transform.localPosition.y;
+
+        
+        if (Mathf.Abs(X) <= 15)
+        {
+            //object is horizontally centered, move it vertically
+            float NewY;
+
+            if (Y <= 0)
+            {
+                NewY = -(Screen.height / 2) - (O.transform as RectTransform).rect.height;
+            }
+            else
+            {
+                NewY = (Screen.height / 2) + (O.transform as RectTransform).rect.height;
+            }
+
+            Y = NewY;
+        }
+        else
+        {
+            //object should move horizontally to the side it is closest to
+            float NewX;
+
+            if (X <= 0)
+            {
+                NewX = -(Screen.width / 2) - (O.transform as RectTransform).rect.width;
+            }
+            else
+            {
+                NewX = (Screen.width / 2) + (O.transform as RectTransform).rect.width;
+            }
+
+            X = NewX;
+        }
+
+        return new Vector2(X, Y);
+    }
+
 	// Update is called once per frame
 	void Update () 
     {
@@ -103,33 +219,41 @@ public class UIBehaviour : MonoBehaviour
                         }
                     }
 
-                    if (MovingObjects.Count == 0)
+                    foreach (FadingUIObject O in new List<FadingUIObject>(FadingObjects))
+                    {
+                        O.Update();
+                        if (O.Completed)
+                        {
+                            FadingObjects.Remove(O);
+                            FinishedFadingObjects.Add(O);
+                        }
+                    }
+
+                    if (MovingObjects.Count == 0 && FadingObjects.Count == 0)
                     {
                         foreach (MovingUIObject O in FinishedObjects) O.Dispose();
                         FinishedObjects.Clear();
+
+                        foreach (FadingUIObject O in FinishedFadingObjects) O.Dispose();
+                        FinishedFadingObjects.Clear();
 
                         GameObject Panel = UIPanels[PendingNewPanel];
                         foreach (Transform child in Panel.transform)
                         {
                             GameObject ChildObject = child.gameObject;
 
-                            float Y = ChildObject.transform.localPosition.y;
-                            float NewY;
-
-                            if (Y <= 0)
+                            if (ChildObject.tag == "Fade")
                             {
-                                NewY = -(Screen.height / 2) - (ChildObject.transform as RectTransform).rect.height;
+                                FadingObjects.Add(new FadingUIObject(ChildObject, true, 0.1f));
                             }
                             else
                             {
-                                NewY = (Screen.height / 2) + (ChildObject.transform as RectTransform).rect.height;
+                                Vector2 NewPos = GetObjectSlideTarget(ChildObject);
+                                Vector2 Target = new Vector2(ChildObject.transform.localPosition.x, ChildObject.transform.localPosition.y);
+
+                                ChildObject.transform.localPosition = new Vector3(NewPos.x, NewPos.y, 0);
+                                MovingObjects.Add(new MovingUIObject(ChildObject, Target, 30f));
                             }
-
-                            Vector3 Pos = ChildObject.transform.localPosition;
-                            Pos.y = NewY;
-                            ChildObject.transform.localPosition = Pos;
-
-                            MovingObjects.Add(new MovingUIObject(ChildObject, Y, 30f));
                         }
 
                         UIPanels[PendingOldPanel].SetActive(false);
@@ -151,7 +275,16 @@ public class UIBehaviour : MonoBehaviour
                         }
                     }
 
-                    if (MovingObjects.Count == 0)
+                    foreach (FadingUIObject O in new List<FadingUIObject>(FadingObjects))
+                    {
+                        O.Update();
+                        if (O.Completed)
+                        {
+                            FadingObjects.Remove(O);
+                        }
+                    }
+
+                    if (MovingObjects.Count == 0 && FadingObjects.Count == 0)
                     {
                         State = UIAnimationStates.Default;
                     }
@@ -172,17 +305,16 @@ public class UIBehaviour : MonoBehaviour
         {
             GameObject ChildObject = child.gameObject;
 
-            float Y = ChildObject.transform.localPosition.y;
-            if (Y <= 0)
+            if (ChildObject.tag == "Fade")
             {
-                Y = -(Screen.height/2) - (ChildObject.transform as RectTransform).rect.height;
+                FadingObjects.Add(new FadingUIObject(ChildObject, false, 0.1f));
             }
             else
             {
-                Y = (Screen.height/2) + (ChildObject.transform as RectTransform).rect.height;
-            }
 
-            MovingObjects.Add(new MovingUIObject(ChildObject, Y, 30f));
+                Vector2 Target = GetObjectSlideTarget(ChildObject);
+                MovingObjects.Add(new MovingUIObject(ChildObject, Target, 30f));
+            }
         }
 
         State = UIAnimationStates.ClearingPanel;
